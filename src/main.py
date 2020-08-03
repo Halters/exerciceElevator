d82573ff185c17fb7aed441f9085d56deb2bfc4f
Hicham.T <hicham.tnacheri@epitech.eu>
2020-08-03 11:53:03 +0200

    feat [MINOR] : add automatic helper and argument parsing

--- src/main.py
+++ src/main.py
@@ -1,59 +1,29 @@
 # Process Digital Tools
-# Author : Hicham TNACHERI OUAZZANI (hicham.tnachgeri@epitech.eu)
+# Author : Hicham TNACHERI OUAZZANI (hicham.tnacheri@epitech.eu)
 # Date : 08/02/2020 at 00:45AM
 # Filename : main.py
 
 import sys
 import json
-from math import *
+import argparse
 from elevator import Elevator
  
 #Define global variables for a better understanding
-ERRORCODE = -1
-SUCCESSCODE = 0
-USAGE = "\nUsage : pyhton main.py \"path to configuration file\""
+
 
 # Prototype : errorHandling(av)
 # Argument : av (array of data)
 # Return value : integer (ERRORCODE or SUCCESSCODE)
 # Description : Manage the potential error with user input
 def errorHandling(av):
-
-        try:
-            if (len(av) != 2):
-                raise IndexError("Too few arguments")
-            if (av[1] != None):
-                file = open(av[1], "r")
-                with open(av[1], "r") as f:
-                    data = json.load(f)
-                #checkJSONFileValidity(data)
-                file.close()
-        except IOError:
-            raise IOError("File doesn't exist.")
-
-
-# Prototype : checkJSONFileValidity(data)
-# Argument : data (JSON packet)
-# Return Value : bool (true or false)
-# Description :  check the format of configuration file
-#def checkJSONFileValidity(data):
-#    keyWords = ["maxFloor", "minFloor", "startFloor"]
-#    
-#    for i in range (len(keyWords)):
-#        try :
-#            retValue = data[keyWords[i]]
-#        except KeyError:
-#            print ("One Value wasn't found.\nJSON value : maxFloor, minFloor, startFloor")
-#    if data['maxFloor'] <= data['minFloor']:
-#        print ("Maximum floor can't be smaller or egal to minimum floor")
-#        return ERRORCODE
-#    if data['minFloor'] >= data['maxFloor']:
-#        print("Minimum floor can't be higher or egal to maximum floor")
-#        return ERRORCODE
-#    if data['startFloor'] > data['maxFloor'] or data['startFloor'] < data['minFloor']:
-#        print("Start Floor can't be smaller than minimum floor or bigger than maximum floor")
-
-
+    try:
+        if (av[1] != None):
+            file = open(av[1], "r")
+            with open(av[1], "r") as f:
+                data = json.load(f)
+            file.close()
+    except IOError:
+        raise IOError("File doesn't exist.")
 
 # Prototype : main(av)
 # Argument : av (array of string)
@@ -63,11 +33,17 @@ def main(av):
     actualElevator = None
     data = None
 
+    parser = argparse.ArgumentParser (
+        description= "Software for an elevator.\nType up to move up and down to move down."
+    )
+    parser.add_argument("filename", type=str, default="config.json", help="Configuration file")
+    arv = parser.parse_args()
     try:
         errorHandling(av)
         with open(av[1], "r") as f:
             data = json.load(f)
-        actualElevator = elevator(data)
+        actualElevator = Elevator(data)
+        #actualElevator.start()
     except Exception as e:
         print(type(e).__name__,":", e)
         return(84)
